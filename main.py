afc20781b182e153d53bdabfa6ece9f7c9f4041a
SingPC <tna121104@gmail.com>
2023-11-10 16:39:17 -0800

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+# mylist=input("Nhập phần tử của list: ").split()
+# print(mylist)
+# pt=int(input('Nhâp số lượng phần tử muốn nhập: '))
+# mylist=[]
+# for i in range(pt):
+#     mylist.append(input(f'Nhập số phần tử thứ {i+1}: ')) # chèn thêm giá trị vào cuối danh sách
+# print(mylist)
+s = list('HOA LU')
+print(f"Số phần tử của danh sách là: {len(s)}")
+a = list(range(10))
+print(a)

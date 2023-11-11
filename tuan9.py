d7628ab59761e8e4336781e1df5ed439b14733a8
SingPC <tna121104@gmail.com>
2023-11-10 16:48:39 -0800

    update

--- tuan9.py
+++ tuan9.py
@@ -5,8 +5,8 @@
 # for i in range(pt):
 #     mylist.append(input(f'Nhập số phần tử thứ {i+1}: ')) # chèn thêm giá trị vào cuối danh sách
 # print(mylist)
-s = list('HOA LU')
-print(f"Số phần tử của danh sách là: {len(s)}")
-a = list(range(10))
-print(a)
-not
+l=[1,2,3,4,5,6]
+for i in range(len(l))
+    print(l[i])
+for i in l:
+    print(i)
